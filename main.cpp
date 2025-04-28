d628e635ae6735c5333269291319a7a8c9f1bf89
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-28 21:57:03 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+
+
+    // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
---
+++ until_2025_04_30/Josephus.cpp
@@ -0,0 +1,38 @@
+﻿//
+// Created by yhj00 on 25. 4. 28.
+//
+
+#include "Josephus.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int n, k;
+    std::cin >> n >> k;
+
+    vector<int> circle;
+    for (int i = 0; i < n; i++) {
+        circle.push_back(i+1);
+    }
+
+    int pointer = 0;
+    vector<int> result;
+    for (int i = 0; i < n; i++) {
+        pointer = (pointer + k - 1) % circle.size();
+        result.push_back(circle[pointer]);
+        circle.erase(circle.begin() + pointer);
+    }
+
+    cout << "<";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i != result.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << ">" << endl;
+
+    return 0;
+}
