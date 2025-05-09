592f31d9e615487ceafe6dd4fb14450d4d761207
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-05-09 16:52:49 +0900

    .

---
+++ until_2025_05_07/bw_chef2.cpp
@@ -0,0 +1,18 @@
+﻿//
+// Created by yhj00 on 25. 5. 7.
+//
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    long long n,a=1,b;
+    cin>>n;
+    while(n--) {
+        cin>>b;
+        a=a*b*2/gcd(a,b*2);
+    }cout<<a;
+}
---
+++ until_2025_05_07/substring_word.cpp
@@ -0,0 +1,33 @@
+﻿//
+// Created by yhj00 on 25. 5. 7.
+//
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string s;
+    cin >> s;
+
+    string answer;
+
+    for (int i = 1; i <= s.size() - 2; i++) {
+        for (int j = 1; j <= s.size() - i - 1; j++) {
+            string a = s.substr(0, i);
+            string b = s.substr(i, j);
+            string c = s.substr(i + j);
+
+            reverse(a.begin(), a.end());
+            reverse(b.begin(), b.end());
+            reverse(c.begin(), c.end());
+
+            string ret = a + b + c;
+            if (answer == "") answer = ret;
+            else if (ret < answer) answer = ret;
+        }
+    }
+
+    cout << answer;
+    return 0;
+}
